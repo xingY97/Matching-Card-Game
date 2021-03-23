078253a677953149d436127a532e3a72b61216ee
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-23 15:26:38 -0400

    CollectionView element added

---
+++ MatchApp/MatchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MatchApp
+//
+//  Created by X Y on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
